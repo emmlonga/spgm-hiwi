3d0741c31ed205f87a0a74e2accad15048ebfbe8
emmlonga <emmlonga@gmail.com>
2017-05-30 01:08:57 +0200

    More changes
    Added new pages, mainly redid the Research page, added teams and
    personel pages.

--- js/myscript.js
+++ js/myscript.js
@@ -21,5 +21,76 @@ $(document).ready(function () {
 myApp.onPageInit('courses', function (page) {
         // Do something here for "index" page
         console.log(page.name + ' initialized');
-        myApp.openPanel('right');
+    var Panel = document.getElementsByClassName('coursePanel')[0];
+    
+    $('.item-inner').click(function(){
+        $('.item-title').removeClass('leftNav_selected')
+        $(this).children('.item-title').addClass("leftNav_selected");
+        openClose();
     });
+    $('#first').trigger('click');    
+});
+
+myApp.onPageInit('researchers', function (page) {
+    // Do something here for "index" page
+    console.log(page.name + ' initialized');
+    loadSlick();
+});
+
+myApp.onPageInit('students', function (page) {
+    // Do something here for "index" page
+    console.log(page.name + ' initialized');
+    loadSlick();
+});
+
+myApp.onPageInit('researchNew', function (page) {
+    // Do something here for "index" page
+    console.log(page.name + ' initialized');
+});
+
+function loadSlick () {
+    $('.your-class').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3      });
+}
+
+function openPanel () {
+    var Panel = document.getElementsByClassName('coursePanel')[0];
+    document.getElementsByClassName('item-title')[0].onclick = function() {
+      if(!Panel.classList.contains('on'))
+      {
+        Panel.classList.add('on');
+      } 
+}
+}
+
+function closePanel () {
+    myApp.closePanel();
+}
+
+function openClose () {
+    var Panel = document.getElementsByClassName('coursePanel')[0];
+    if(!Panel.classList.contains('on')){
+        Panel.classList.add('on');
+      } else {
+        Panel.classList.remove('on');
+        setTimeout('document.getElementsByClassName("coursePanel")[0].classList.add("on")', 600);
+      }
+}
+
+function toggleFilter () {
+    var Filter = document.getElementsByClassName('pLeft')[0];
+    if(!Filter.classList.contains('on')){
+        Filter.classList.add('on');
+      } else {
+        Filter.classList.remove('on');
+      }
+}
+
+$('.item-inner').click(function(){
+    $('.item-title').removeClass('leftNav_selected')
+    $(this).children('.item-title').addClass("leftNav_selected");
+    openClose();
+});
+
---
+++ js/team.js
@@ -0,0 +1,42 @@
+/**TEAM**/
+
+var R1 = {
+    name: "Aljoscha Hermann",
+    title: "M.Sc",
+    room: "MW 3311 / Hochbrück Parkring 35 Room 3.3.08",
+    email: "hermann@lfe.mw.tum.de",
+    tel: "+49 89 289 - 10353",
+    research: "Biomechanics & Materials Science & Physiology",
+    picture: "http://www.spgm.mw.tum.de/fileadmin/_processed_/csm_Mitarbeiter_Hermann_3df97fead0.jpg",
+    /** options: "completed", "attending", "remaining". "remaing is standard"**/
+    hours: "Tuesdays, 09:00 till 10:00",
+    resume: [
+        ["06.2015", "Research Associate, Professorship of Sport Equipment and Materials, Technical University Munich"]
+    ],
+    publications: [
+        "Nusser, M., Hermann, A. & Senner, V. (2016). Artificial Knee Joint and Ski Load Simulator for the Evaluation of Knee Braces and Ski Bindings. Proceedia Engineeing."
+    ],
+    project: [
+        "KNEEMAX",
+        "Development and application of a knee simulator to determine the forces occurring in the knee under typical stress situations in alpine skiing.",
+        "Functional textiles measure movement and muscle activation during skiing."
+    ],
+    student: [
+        "Development and construction of a module for the detection of motion parameters during exercise",
+        "Automation of a Biomechanical Knee Surrogat: Implementation of the external load application",
+        "Camera Based Calculations of the Elongation of the Ligaments in a Physical Knee Model",
+        "Design, Motorization, Evaluation of the Muscular System in a Human Knee Model •	Concept Development for a Binding to Determine the Loads on a Skier During Alpine Skiing",
+        "Effects of an Isometric Cocontraction of the Thigh Muscles on the Anterior Tibial Translation",
+        "Muscle activation ratios of the m. quadrizeps femoris and the hamstring muscles in recreational skiing: An explorative study",
+        "Investigation of Different Measurement Methods with Regards to their Qualification to Measure Knee Angle During Skiing",
+        "Developing an Underpant for Skiing with Integrated Sensors to Measure the Knee Angle and the Muscle Activity",
+        "Development of Technical Textiles for the Recording of Movement and Muscle Activity During Alpine Skiing"
+    ],
+    lectures: [
+        "Ergonomisches Praktikum, Muskelphysiologie",
+        "CATIA Basics und Innovationsbewertung – practical curse affiliated to the lecture Sporttechnologiesche Kompetenz"
+    ],
+    doctoral: [
+        ["Development of Algorithms for Mechatronic Ski Bindings to Reduce Knee Injuries in Alpine Skiing", "06.2016", "Faculty Mechanical Engineering", "Dr.-Ing. Veit Senner"]
+    ]
+}
